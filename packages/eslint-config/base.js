f2b17fb41610a8db8cf7eb8b8e42f35c37a4c73e
ianlavapiez <ianlavapiez@gmail.com>
2025-09-21 10:54:25 +0800

    Update ESLint configuration to include import plugin and resolver settings; add dependencies for eslint-import-resolver-typescript and eslint-plugin-import in package.json.

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,13 +1,13 @@
-import js from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
-import onlyWarn from "eslint-plugin-only-warn"
-import turboPlugin from "eslint-plugin-turbo"
-import tseslint from "typescript-eslint"
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+import importPlugin from "eslint-plugin-import";
 
 /**
  * A shared ESLint configuration for the repository.
  *
- * @type {import("eslint").Linter.Config}
+ * @type {import("eslint").Linter.Config[]}
  * */
 export const config = [
   js.configs.recommended,
@@ -16,17 +16,62 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      import: importPlugin,
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: "./tsconfig.json",
+        },
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      },
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
-    },
-  },
-  {
-    plugins: {
-      onlyWarn,
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "next{,/**}",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "react",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "@packages/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["react", "next"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "import/no-unresolved": "error",
     },
   },
   {
     ignores: ["dist/**"],
   },
-]
+];
